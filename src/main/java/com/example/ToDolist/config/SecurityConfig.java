2a1248712db0720e1696aa4e8508a4d66f08be3c
mtzhao0827 <justting_0827@outlook.com>
2024-08-29 14:57:49 +0800

    实现了密码加密

---
+++ src/main/java/com/example/ToDolist/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.ToDolist.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain springWebFilterChain(
+            HttpSecurity http
+    ) throws Exception {
+
+        return http.httpBasic( AbstractHttpConfigurer::disable)
+                .sessionManagement(c ->
+                        c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests(c -> c
+                        .anyRequest().permitAll()
+                )
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
